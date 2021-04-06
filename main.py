dbae4fda4822b6c5270f27e4af4a831df3df9cd4
Replit user <replituser@example.com>
2021-04-06 00:46:07 +0000

    added apis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 import random
+import requests
 app = Flask(__name__)
 
 from flask import render_template
@@ -14,8 +15,17 @@ def hello(name=None):
 def hello_hi():
   return 'Hi!'
 
+@app.route('/planes')
+def planes():
+  r = requests.get('https://api.creativecommons.engineering/v1/images?q=cirrussr22')
+  print(str(r.status_code))
+  print(r.text)
+  x = r.json()
+  return render_template('planes.html', name="Vinay", data = x)
+
+
 
 if __name__ == "__main__": # Makes sure this is the main process
-  app.run( # Starts the site
-  host='0.0.0.0', # EStablishes the host, required for repl to detect the site
+  app.run( # Starts the site 
+  host='0.0.0.0', # Establishes the host, required for repl to detect the site
   port=random.randint(2000, 9000)) # Randomly select the port the machine hosts on
